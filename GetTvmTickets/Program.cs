4f00151ad8648227e19af0459bcdef7a9c8d5c05
Adrian Sims <adriansims62781@gmail.com>
2018-01-24 00:13:09 +0000

    Add project files.

---
+++ GetTvmTickets/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace GetTvmTickets
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+                if (args.Length != 1)
+                {
+                    throw new Exception($"You must supply one filename (RCS_R_T...)");
+                }
+                var doc = XDocument.Load(args[0]);
+                var ns = doc.Root.GetDefaultNamespace();
+                var nonTvmTickets1 = doc.Descendants(ns + "FTOT").Select(x=>x.Descendants(ns + "Channel"));
+                var nonTvmTickets = doc.Descendants(ns + "FTOT").Select(x => new { TicketCode = x.Attribute("t").Value, ChannelList = x.Descendants(ns + "Channel").Select(y => y.Attribute("ch").Value)}).Where(z=>!z.ChannelList.Contains("00004")).Select(a=>a.TicketCode).ToList();
+                nonTvmTickets.ForEach(x => Console.WriteLine(x));
+            }
+            catch (Exception ex)
+            {
+                var codeBase = System.Reflection.Assembly.GetExecutingAssembly().CodeBase;
+                var progname = Path.GetFileNameWithoutExtension(codeBase);
+                Console.Error.WriteLine(progname + ": Error: " + ex.Message);
+            }
+
+        }
+    }
+}
---
+++ gitmehappy.cmd
@@ -0,0 +1,8 @@
+rem echo off
+git init
+git add GetTvmTickets/Program.cs
+git add GetTvmTickets/App.config
+git add GetTvmTickets/Properties/AssemblyInfo.cs
+git add GetTvmTickets/GetTvmTickets.csproj
+git add GetTvmTickets.sln
+git add .gitignore
